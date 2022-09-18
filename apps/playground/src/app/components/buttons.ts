5ad059e9e976acac927dfa0a065344b4d2e3f038
wolffparkinson <krishanu1296@gmail.com>
2022-09-18 19:36:43 +0530

    fix(buttons): add custom decorators to pipes

--- apps/playground/src/app/components/buttons.ts
+++ apps/playground/src/app/components/buttons.ts
@@ -1,17 +1,23 @@
-import { IsInt } from 'class-validator';
-import { Injectable, ValidationPipe } from '@nestjs/common';
+import { IsNumber } from 'class-validator';
+import { Injectable,  ValidationPipe, ValidationPipeOptions } from '@nestjs/common';
 import { Button, ButtonContext, ComponentParam, Ctx } from 'necord';
 import { codeBlock } from 'discord.js';
+import { Type } from 'class-transformer';
+
+export const vPipe = (options:ValidationPipeOptions) => {
+  return new ValidationPipe({validateCustomDecorators:true,...options})
+}
 
 export class ButtonOptions {
-  @IsInt()
+  @IsNumber()
+  @Type(()=>Number)
   arg1!:number  
 }
 
 @Injectable()
 export class Buttons {
   @Button('hello/:arg1')
-  onHello(@Ctx() [interaction]:ButtonContext,@ComponentParam(new ValidationPipe()) params: ButtonOptions){
+  onHello(@Ctx() [interaction]:ButtonContext,@ComponentParam(new ValidationPipe({validateCustomDecorators:true,transform:true})) params: ButtonOptions){
     return interaction.reply({
       ephemeral:true,
       content:codeBlock('JSON',JSON.stringify(params))
